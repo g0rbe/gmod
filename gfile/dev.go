5394c9414276bb70fdcf1803ae299df5c5833326
g0rbe <mail@danielgorbe.com>
2024-01-03 07:23:07 +0100

    feat(gfile): add DevZero and DevNull

---
+++ gfile/dev.go
@@ -0,0 +1,27 @@
+package gfile
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	DevZero io.Reader
+	DevNull io.Writer
+)
+
+func init() {
+
+	var err error
+
+	DevZero, err = os.OpenFile("/dev/zero", os.O_RDONLY, 0)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open /dev/zero: %s", err))
+	}
+
+	DevNull, err = os.OpenFile("/dev/null", os.O_WRONLY, 0)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open /dev/null: %s", err))
+	}
+}
