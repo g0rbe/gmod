cd2e1d63c11b62285d9e817c5a5650dd03fcf672
g0rbe <mail@danielgorbe.com>
2024-01-03 08:34:52 +0100

    feat(gfile): add file locking shred functions

--- gfile/shred.go
+++ gfile/shred.go
@@ -7,16 +7,9 @@ import (
 	"os"
 )
 
-// Shred overwrites file in path for n times.
-// Writes random data if random is true, else zeroes.
-// Removes the file at the end if remove is true.
-func Shred(path string, n int, random bool, remove bool) (int64, error) {
-
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_SYNC, 0)
-	if err != nil {
-		return -1, fmt.Errorf("failed to open: %w", err)
-	}
-	defer file.Close()
+// shred overwrite file for n times with random (if random is true) or zeroes.
+// Seek to the beginning of file at every iteration.
+func shred(file *os.File, n int, random bool) (int64, error) {
 
 	stat, err := file.Stat()
 	if err != nil {
@@ -47,10 +40,64 @@ func Shred(path string, n int, random bool, remove bool) (int64, error) {
 		}
 	}
 
-	if remove {
-		file.Close()
-		err = os.Remove(path)
-	}
+	return totalWritten, nil
+}
 
-	return totalWritten, err
+// Shred overwrites file in path for n times.
+// Writes random data if random is true, else zeroes.
+func Shred(path string, n int, random bool) (int64, error) {
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_SYNC, 0)
+	if err != nil {
+		return -1, fmt.Errorf("failed to open: %w", err)
+	}
+	defer file.Close()
+
+	return shred(file, n, random)
+}
+
+// ShredLock apply a nonblocking write lock with FlockWrite and overwrites file in path for n times.
+// Writes random data if random is true, else zeroes.
+//
+// This function returns error if other lock is applied on path.
+func ShredLock(path string, n int, random bool) (int64, error) {
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_SYNC, 0)
+	if err != nil {
+		return -1, fmt.Errorf("failed to open: %w", err)
+	}
+	defer file.Close()
+
+	// Apply a blocking lock.
+	err = FlockWrite(file.Fd())
+	if err != nil {
+		return -1, fmt.Errorf("failed to lock: %w", err)
+	}
+	// Unlock at return
+	defer FlockUnlock(file.Fd())
+
+	return shred(file, n, random)
+}
+
+// ShredLock apply a blocking write lock with FlockWriteWait and overwrites file in path for n times.
+// Writes random data if random is true, else zeroes.
+//
+// This function blocks until other lock is applied on path.
+func ShredLockWait(path string, n int, random bool) (int64, error) {
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_SYNC, 0)
+	if err != nil {
+		return -1, fmt.Errorf("failed to open: %w", err)
+	}
+	defer file.Close()
+
+	// Apply a nonblocking lock.
+	err = FlockWriteWait(file.Fd())
+	if err != nil {
+		return -1, fmt.Errorf("failed to lock: %w", err)
+	}
+	// Unlock at return
+	defer FlockUnlockWait(file.Fd())
+
+	return shred(file, n, random)
 }
--- gfile/shred_test.go
+++ gfile/shred_test.go
@@ -14,26 +14,22 @@ func TestShred(t *testing.T) {
 		t.Fatalf("Failed to write %s: %s\n", testfile, err)
 	}
 
-	written, err := Shred(testfile, 3, true, false)
+	written, err := Shred(testfile, 3, true)
 	if err != nil {
 		t.Fatalf("Failed to shred with random: %s\n", err)
 	}
 
 	t.Logf("Written bytes: %d\n", written)
 
-	written, err = Shred(testfile, 1, false, true)
+	written, err = Shred(testfile, 1, false)
 	if err != nil {
 		t.Fatalf("Failed to shred with zero: %s\n", err)
 	}
 
 	t.Logf("Written bytes: %d\n", written)
 
-	exist, err := IsExists(testfile)
+	err = os.Remove(testfile)
 	if err != nil {
-		t.Fatalf("Failed to check if %s is exist: %s\n", testfile, err)
-	}
-
-	if exist {
-		t.Fatalf("%s should not exist\n", testfile)
+		t.Fatalf("Failed to remove %s: %s\n", testfile, err)
 	}
 }
