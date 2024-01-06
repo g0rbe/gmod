f3cd5eaeab6789d2bbbf498d6c5b7d7e864deae9
g0rbe <mail@danielgorbe.com>
2024-01-06 03:07:53 +0100

    feat(gfile): add TruncatePath()

--- gfile/truncate.go
+++ gfile/truncate.go
@@ -20,3 +20,25 @@ func Truncate(file *os.File) error {
 
 	return file.Truncate(0)
 }
+
+// TruncatePath overwrites the file at path with shred for ShredIterations times and truncates to 0.
+func TruncatePath(path string) error {
+
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_SYNC, 0)
+	if err != nil {
+		return fmt.Errorf("failed to open %s: %w", path, err)
+	}
+	defer file.Close()
+
+	_, err = shred(file, ShredIterations, true)
+	if err != nil {
+		return fmt.Errorf("failed to shred: %w", err)
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return fmt.Errorf("failed to seek: %w", err)
+	}
+
+	return file.Truncate(0)
+}
